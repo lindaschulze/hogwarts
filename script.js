e727279d914ce5bd41eef5f53773ea50492eef8a
lindaschulze <linda.schulze@mail.com>
2025-03-04 21:43:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,43 @@ async function startCamera() {
     }
 }
 
+async function detectFace() {
+    await faceapi.nets.tinyFaceDetector.loadFromUri('https://cdn.jsdelivr.net/npm/face-api.js/weights');
+    const video = document.getElementById("video");
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    setInterval(async () => {
+        const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions());
+        if (detections) {
+            const x = detections.box.x;
+            const y = detections.box.y - 50;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = "brown";
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + 60, y + 50);
+            ctx.lineTo(x - 60, y + 50);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }, 100);
+}
+
+function sortHouse() {
+    const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+    const chosenHouse = houses[Math.floor(Math.random() * houses.length)];
+    document.getElementById("house").innerText = `Du bist in ${chosenHouse}!`;
+
+    const speech = new SpeechSynthesisUtterance(`You belong to ${chosenHouse}!`);
+    speechSynthesis.speak(speech);
+}
+
+video.addEventListener("play", detectFace);
+
+setTimeout(sortHouse, 5000);
+
 startCamera();
